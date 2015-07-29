db353820141f581a75ac76bd8bde75486bf2bed6
alpipego <alpipego@gmail.com>
2015-07-29 09:17:23 +0200

    initial commit

---
+++ quan-blockquote.php
@@ -0,0 +1,37 @@
+<?php
+
+    /**
+     *   Plugin Name: Quan Blockquote Shortcode
+     *   Plugin URI: http://www.quandigital.com/
+     *   Description: Adds blockquote/cite shortode
+     *   Version: 1.0.0
+     *   License: MIT
+     */
+    
+    defined( 'ABSPATH' ) or die();
+
+    function quanBlockquote($atts, $content = null)
+    {
+        if (!is_null($content)) {
+            if (isset($atts['name'])) {
+                $src = $atts['name'];
+                if (isset($atts['source'])) {
+                    $src = '<a href="' . $atts['source'] . '" target="_blank">' . $atts['name'] . '</a>';
+                }
+            } else {
+                if (isset($atts['source'])) {
+                    $src = '<a href="' . $atts['source'] . '" target="_blank">' . $atts['source'] . '</a>';
+                }
+            }
+
+            if (isset($src)) {
+                return '<blockquote class="quan-blockquote"><p>' . $content . '</p><footer><cite>' . $src . '</cite><footer></blockquote>';    
+            }
+
+            return '<blockquote class="quan-blockquote"><p>' . $content . '</p></blockquote>';
+        }
+
+        return;
+    }
+
+    add_shortcode( 'quote', 'quanBlockquote' );
