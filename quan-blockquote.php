40ecc81e3df0d45901960810b175bdc997065b51
alpipego <alpipego@gmail.com>
2015-09-16 18:40:07 +0200

    cosmetic changes, version number, author, plugin url

--- quan-blockquote.php
+++ quan-blockquote.php
@@ -1,37 +1,39 @@
 <?php
 
-    /**
-     *   Plugin Name: Quan Blockquote Shortcode
-     *   Plugin URI: http://www.quandigital.com/
-     *   Description: Adds blockquote/cite shortode
-     *   Version: 1.0.0
-     *   License: MIT
-     */
-    
-    defined( 'ABSPATH' ) or die();
+/**
+ *   Plugin Name: Quan Blockquote Shortcode
+ *   Plugin URI: https://github.com/quandigital/wp-quan-blockquote
+ *   Author: Quan Digital GmbH
+ *   Author URI: http://www.quandigital.com/
+ *   Description: Adds blockquote/cite shortode
+ *   Version: 1.0.1
+ *   License: MIT
+ */
 
-    function quanBlockquote($atts, $content = null)
-    {
-        if (!is_null($content)) {
-            if (isset($atts['name'])) {
-                $src = $atts['name'];
-                if (isset($atts['source'])) {
-                    $src = '<a href="' . $atts['source'] . '" target="_blank">' . $atts['name'] . '</a>';
-                }
-            } else {
-                if (isset($atts['source'])) {
-                    $src = '<a href="' . $atts['source'] . '" target="_blank">' . $atts['source'] . '</a>';
-                }
+defined('ABSPATH') or die();
+
+function quanBlockquote($atts, $content = null)
+{
+    if (!is_null($content)) {
+        if (isset($atts['name'])) {
+            $src = $atts['name'];
+            if (isset($atts['source'])) {
+                $src = '<a href="' . $atts['source'] . '" target="_blank">' . $atts['name'] . '</a>';
             }
-
-            if (isset($src)) {
-                return '<blockquote class="quan-blockquote"><p>' . $content . '</p><footer><cite>' . $src . '</cite><footer></blockquote>';    
+        } else {
+            if (isset($atts['source'])) {
+                $src = '<a href="' . $atts['source'] . '" target="_blank">' . $atts['source'] . '</a>';
             }
-
-            return '<blockquote class="quan-blockquote"><p>' . $content . '</p></blockquote>';
         }
 
-        return;
+        if (isset($src)) {
+            return '<blockquote class="quan-blockquote"><p>' . $content . '</p><footer><cite>' . $src . '</cite><footer></blockquote>';    
+        }
+
+        return '<blockquote class="quan-blockquote"><p>' . $content . '</p></blockquote>';
     }
 
-    add_shortcode( 'quote', 'quanBlockquote' );
+    return;
+}
+
+add_shortcode('quote', 'quanBlockquote');
